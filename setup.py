e4e33b5656d2331cb84102cb899737911afbdcd2
VatsalPatel18 <vatsal1804@gmail.com>
2025-12-02 12:24:05 +0530

    Bump version to 1.0.7 for PyPI release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vatsalpatel18_rag_llm_metabolomics",
-    version="0.1.0",
+    version="1.0.7",
     description="A tool for RAG-LLM performance analysis on research papers (metabolomics) with support for GPU and CPU modes.",
     author="Vatsal Patel",
     packages=find_packages(),
